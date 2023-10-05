f7b33471bef827c8c047a9831da2b6f3c8fff3b5
JohnWoocur <johnwoocur@gmail.com>
2023-10-05 19:55:44 +0530

    Initial Release

---
+++ First.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //It is the library file Declaration
+using namespace std; //This program is based on the Built in Name space Standards
+int main() //Main Methods
+{
+    string name;
+    cout<<"Hello world!"<<endl<<"Enter Your Name:\n";
+    cin>>name;
+    cout<<"Hello "<<name;
+    return 0;
+}
